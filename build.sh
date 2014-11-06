275ffc3f5ad522b86b7cb7357ef81b8359f3cd4b
jint <jesse@spire.com>
2014-11-06 23:18:20 +0000

    Cleaned up unnecessary files and reorganized directory

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Script to manually compile PREDICT
-#
-cc -g -Wall -fomit-frame-pointer predict.c -lm -lncurses -pthread -o predict
--- clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Script to prepare for a clean distribution.
-#
-rm -f predict
-echo "Done!"
