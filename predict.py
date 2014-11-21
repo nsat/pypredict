d48be8f428a594c3ad40fed16ac92c5fc5a37a8a
jint <jesse@nanosatisfi.com>
2014-11-20 16:50:21 -0800

    General API improvements + added better extract-transform-load of tle and qth parameters to Observer.__init__ + make default qth explicit instead of relying on behavior of compiled C extension + replaced custom pass iterator class with simple generator + added endtime argument to pass generator to allow for convenient list comprehensions + modified transit wrapper class for convenient use by library importers + modified max_elevation to calculate maximum instead of relying on predict

--- predict.py
+++ predict.py
@@ -1,6 +1,8 @@
 import os
 import time
 import urllib2
+from os import path
+from math import ceil
 from datetime import datetime
 from cpredict import quick_find, quick_predict, PredictException
 
@@ -34,59 +36,69 @@ def host_qth():
     return qth("~/.predict/predict.qth")
 
 class Observer():
-    def __init__(self, tle, qth=None):
-        self.tle = tle.rstrip().split('\n')
-        self.qth = qth
+    def __init__(self, tle, qth="~/.predict/predict.qth"):
+        ## ETL tle and qth data
+        self.tle = tle.rstrip().split('\n') if isinstance(tle, basestring) else tle
+        if isinstance(qth, basestring):
+            raw = open(path.expanduser(qth)).readlines()
+            lines = [l.strip() for l in raw]
+            assert len(lines) == 4, "qth file '%s' must contain exactly 4 lines (name, lat, long, alt)" % qth
+            qth = lines[1:] # remove name
+        # Attempt conversion to format required for predict.quick_find
+        assert len(qth) == 3, "qth must be (lat, long, alt) as (float, float, int)" 
+        self.qth = (float(qth[0]), float(qth[1]), int(qth[2]))
 
-    def observe(self, at=None):
-        at = at or time.time()
-        if self.qth:
-            return quick_find(self.tle, at, self.qth)
-        else:
-            return quick_find(self.tle, at)
+    def observe(self, at = None):
+        at = at if at != None else time.time()
+        return quick_find(self.tle, at, self.qth)
 
-    def passes(self, at = time.time()):
-        return PassGenerator(self.tle, at, self.qth)
+    # Returns a generator of passes occuring entirely between 'after' and 'before' (epoch)
+    def passes(self, after=None, before=None):
+        after = after or time.time()
+        crs = after
+        while True:
+            transit = quick_predict(self.tle, crs, self.qth[0:3])
+            start = transit[0]['epoch']
+            end = transit[-1]['epoch']
+            t = Transit(self.tle, self.qth, start, end) # un/repacking for convenient external API
+            if (before != None and before < t.end):
+                break
+            if (t.start > after):
+                yield t
+            # Need to advance time cursor sufficiently far so predict doesn't yield same pass
+            crs = t.end + 60     #seconds seems to be sufficient
 
-# Transit is a thin wrapper around the array of dictionaries returned by cpredict.quick_predict
+# Transit is a convenience class representing a pass of a satellite over a groundstation.
+# It is both an internal class (Observer.passes returns an iterator of transits) and an external
+# class (importers of the library may instantiate).  
 class Transit():
-    def __init__(self, observations):
-        self.points = observations
+    def __init__(self, tle, qth, start, end):
+        self.observer = Observer(tle, qth)
+        self.start = start
+        self.end = end
+        # Let Observer.__init__ handle ETL for us.  Re-use!
+        self.qth = self.observer.qth
+        self.tle = self.observer.tle
 
-    def start_time(self):
-        return self.points[0]['epoch']
+    # return the name of the satellite transiting
+    def satellite(self):
+        return self.observer.tle[0]
 
-    def end_time(self):
-        return self.points[-1]['epoch']
-
-    # TODO: Verify quick_predict returns observation at peak of transit
     def max_elevation(self):
-        return max([p['elevation'] for p in self.points])
+        #TODO: Optimize (or at least cache) this.  Also, sub-second granularity?
+        return max([self.observer.observe(t)['elevation'] for t in range(int(self.start), int(ceil(self.end)))])
 
-    def __getitem__(self, key):
-        return self.points[key]
+    def duration(self):
+        return self.end - self.start
 
-class PassGenerator():
-    def __init__(self, tle, ts=None, qth=None):
-        self.tle  = tle
-        self.time = ts or time.time()
-        self.qth  = qth
-
-    def __iter__(self):
-        return self
-
-    # Python 3 compatibility
-    def __next__(self):
-        return self.next()
-
-    def next(self):
-        if self.qth:
-            p = Transit(quick_predict(self.tle, self.time, self.qth))
+    def at(self, t):
+        if self.start <= t <= self.end:
+            return self.observer.observe(timestamp)
         else:
-            p = Transit(quick_predict(self.tle, self.time))
-        #HACK: If the timestamp passed to quick_predict is within a pass (or within an unclear
-        #      delta of one of the endpoints, it will return that pass.  To generate the next
-        #      pass, we have to advance the requested time.  It's not clear how much of a buffer
-        #      we need, but the following seems to work.  Single-digits amounts weren't enough.
-        self.time = p.end_time() + 60
-        return p
+            raise LookupError("time %f outside of transit window [%f, %f]" % (t, self.start, self.end))
+
+    def __repr__(self):
+        return "Transit%s"%(self.tle, self.qth, self.start, self.end).__repr__()
+
+    def __str__(self):
+        return str((self.start, self.end, self.max_elevation(), self.duration()))
