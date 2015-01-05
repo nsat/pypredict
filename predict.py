fbfa9b95b63136675a8e1aad5c1b8c215710d146
jint <jesse@nanosatisfi.com>
2015-01-04 22:59:20 -0800

    Big reorganization in face of external changes and more use cases.

--- predict.py
+++ predict.py
@@ -6,116 +6,120 @@ from math import ceil
 from datetime import datetime
 from cpredict import quick_find, quick_predict, PredictException
 
-def tle(norad_id):
+def tle(sat_id):
     try:
-        res = urllib2.urlopen("http://tle.nanosatisfi.com/%s" % str(norad_id))
+        res = urllib2.urlopen("http://tle.nanosatisfi.com/%s" % str(sat_id))
         if res.getcode() != 200:
             raise urllib2.HTTPError("Unable to retrieve TLE from tle.nanosatisfi.com. HTTP code(%s)" % res.getcode())
         return res.read().rstrip()
     except Exception as e:
         raise PredictException(e)
 
-def qth(qth_path):
+def massage_tle(tle):
     try:
-        qth_path = os.path.abspath(os.path.expanduser(qth_path))
-        with open(qth_path) as qthfile:
-            raw = [l.strip() for l in qthfile.readlines()]
-            assert len(raw) == 4, "qth file '%s' must contain exactly 4 lines (name, lat(N), long(W), alt(m))" % qth_path
-            # Attempt conversion to format required for predict.quick_find
-            return (float(raw[1]), float(raw[2]), int(raw[3]))
-    except IOError as e:
-        raise PredictException("Unable to open '%s' (%s)" % (qth_path, str(e)))
-    except AssertionError as e:
-        raise PredictException(str(e))
+        # TLE may or may not have been split into lines already
+        if isinstance(tle, basestring):
+            tle = tle.rstrip().split('\n')
+        assert len(tle) == 3, "TLE must be 3 lines, not %d: %s" % (len(tle), tle)
+        return tle
+        #TODO: print a warning if TLE is 'too' old
+    except Exception as e:
+        raise PredictException(e)
+
+def massage_qth(qth):
+    try:
+        # QTH may or may not have been converted into 3-tuple
+        if isinstance(qth, basestring):
+            qth = qth.rstrip().split('\n')
+            assert len(qth) == 4, "'%s' must contain exactly 4 lines: name, lat(N), long(W), alt(m)" % qth
+            qth = qth[1:] # remove name
+        assert len(qth) == 3, "%s must consist of exactly three elements: (lat(N), long(W), alt(m))" % qth
+        # Attempt conversion to format required for predict.quick_find
+        return (float(qth[0]), float(qth[1]), int(qth[2]))
     except ValueError as e:
-        raise PredictException("Unable to process '%s' (%s)" % (qth_path, str(e)))
+        raise PredictException("Unable to convert '%s' (%s)" % (qth, str(e)))
     except Exception as e:
         raise PredictException(e)
 
 def host_qth():
-    return qth("~/.predict/predict.qth")
+    try:
+        qth_path = os.path.abspath(os.path.expanduser("~/.predict/predict.qth"))
+        with open(qth_path) as qthfile:
+            return massage_qth(qthfile.read())
+    except IOError as e:
+        raise PredictException("Unable to open '%s' (%s)" % (qth_path, str(e)))
 
 class Observer():
-    def __init__(self, tle, qth="~/.predict/predict.qth"):
-        ## ETL tle and qth data
-        self.tle = tle.rstrip().split('\n') if isinstance(tle, basestring) else tle
-        if isinstance(qth, basestring):
-            with open(path.expanduser(qth)) as fp:
-                lines = [l.strip() for l in fp.readlines()]
-                assert len(lines) == 4, "qth file '%s' must contain exactly 4 lines (name, lat, long, alt)" % qth
-                qth = lines[1:] # remove name
-        # Attempt conversion to format required for predict.quick_find
-        assert len(qth) == 3, "qth must be (lat, long, alt) as (float, float, int)" 
-        self.qth = (float(qth[0]), float(qth[1]), int(qth[2]))
+    def __init__(self, tle, qth=None):
+        self.qth = host_qth() if (qth is None) else massage_qth(qth)
+        self.tle = massage_tle(tle)
 
-    def observe(self, at = None):
+    def observe(self, at=None):
         at = time.time() if (at is None) else at
         return quick_find(self.tle, at, self.qth)
 
-    # Returns a generator of passes occuring entirely between 'start' and 'stop' (epoch)
+    # Returns a generator of transits that overlap the 'start' to 'stop' time interval
     def passes(self, start=None, stop=None):
         start = time.time() if (start is None) else start
-        crs = start
+        ts = start
         while True:
-            transit = quick_predict(self.tle, crs, self.qth)
-            # un/repacking for convenient external API
-            t = Transit(self.tle, self.qth, start=transit[0]['epoch'], end=transit[-1]['epoch']) 
-            if (stop != None and t.end > stop):
+            transit = quick_predict(self.tle, ts, self.qth)
+            t = Transit(self.tle, self.qth, start=transit[0]['epoch'], end=transit[-1]['epoch'])
+            if (stop != None and t.start >= stop):
                 break
-            if (t.start > start):
+            if (t.end > start):
                 yield t
-            # Need to advance time cursor sufficiently far so predict doesn't yield same pass
-            crs = t.end + 60     #seconds seems to be sufficient
+            # Need to advance time cursor so predict doesn't yield same pass
+            ts = t.end + 60     #seconds seems to be sufficient
 
 # Transit is a convenience class representing a pass of a satellite over a groundstation.
-# It is both an internal class (Observer.passes returns an iterator of transits) and an external
-# class (importers of the library may instantiate).  
 class Transit():
     def __init__(self, tle, qth, start, end):
-        self.engine = Observer(tle, qth)
-        self.start = start
         self.end = end
-        # Let Observer.__init__ handle ETL for us.  Re-use!
+        self.start = start
+        # tle & qth may not be valid here (since library importer can instantiate a Transit)
+        # We re-use the Observer class to perform input conversion
+        self.engine = Observer(tle, qth)
         self.qth = self.engine.qth
         self.tle = self.engine.tle
 
-    # return the name of the satellite transiting
-    def satellite(self):
-        return self.engine.tle[0]
-
-    # return timestamp within epsilon seconds of maximum elevation
+    # return observation within epsilon seconds of maximum elevation
     def peak(self, epsilon=0.1):
-        crs =  (self.end + self.start)/2
+        ts =  (self.end + self.start)/2
         step = (self.end - self.start)
         while (step > epsilon):
             step /= 4
-            # Ascend the gradient
+            # Ascend the gradient at this step size
             direction = None
             while True:
-                mid   = self.engine.observe(crs)['elevation']
-                left  = self.engine.observe(crs - step)['elevation']
-                right = self.engine.observe(crs + step)['elevation']
-                # Stop if left or right exceed transit window
-                if (((crs - step) <= self.start) or (self.end <= (crs + step))):
-                    break
-                # Stop if we're at a peak
+                mid   = self.engine.observe(ts)['elevation']
+                left  = self.engine.observe(ts - step)['elevation']
+                right = self.engine.observe(ts + step)['elevation']
+                # Break if we're at a peak
                 if (left <= mid >= right):
                     break
-                gradient = -1 if (left > right) else 1
-                # Stop if we've stepped over a peak
-                if direction and direction != gradient:
+                # Ascend up slope
+                slope = -1 if (left > right) else 1
+                # Break if we stepped over a peak (switched directions)
+                if direction is None:
+                    direction = slope
+                if direction != slope:
+                    break
+                # Break if stepping would take us outside of transit
+                next_ts = ts + (direction * step)
+                if (next_ts < self.start) or (next_ts > self.end):
                     break
                 # Step towards the peak
-                direction = gradient
-                crs += (direction * step)
-        return crs
+                ts = next_ts
+        return self.at(ts)
 
     # Generator that returns an observation every 'step' seconds
     def points(self, step=15.0):
         # Number of steps that fit within this pass
-        number = int(self.duration() / step)
-        offset = (self.duration() - (number * step))*2
-        for i in range(0, number):
+        count = int(self.duration() / step)
+        # Center the observations over the interval
+        offset = (self.duration() - (count * step))/2
+        for i in range(0, count):
             yield self.at(self.start + offset + (i * step))
 
     def duration(self):
@@ -127,8 +131,5 @@ class Transit():
         else:
             raise LookupError("time %f outside of transit window [%f, %f]" % (t, self.start, self.end))
 
-    def __repr__(self):
-        return "Transit%s"%(self.tle, self.qth, self.start, self.end).__repr__()
-
     def __str__(self):
-        return str((self.start, self.end, self.at(self.peak())['elevation'], self.duration()))
+        return str((self.start, self.end, self.peak()['elevation'], self.duration()))
