05e30fc63b506bc02faa5454ac1f96ced6920cae
Jeremiah Matthey <jeremiah.matthey@spire.com>
2020-05-11 23:21:55 +0100

    Guard against floating point errors in at function

--- predict.py
+++ predict.py
@@ -169,7 +169,7 @@ class Transit():
     def duration(self):
         return self.end - self.start
 
-    def at(self, t):
-        if t < self.start or t > self.end:
+    def at(self, t, epsilon=0.001):
+        if t < (self.start - epsilon) or t > (self.end + epsilon):
             raise PredictException("time %f outside transit [%f, %f]" % (t, self.start, self.end))
         return observe(self.tle, self.qth, t)
