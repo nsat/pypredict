b0178e7042985de114d24402d6cc01bd66eb9860
Nick Pascucci <82097533+nick-pascucci-spire@users.noreply.github.com>
2022-02-02 15:51:03 +0000

    tests: add tox integration and build wheels on ci (#53)
    * test: update test data and integrate tox
    
    * ci: add config for building wheels & running tox
    
    * tests: downgrade cibuildwheel to get py2 support
    
    * ci: trigger
    
    * ci: quote python versions, don't fail fast
    
    * ci: build wheels once and store artifacts
    
    * ci: change artifact path to dist to match Makefile
    
    * ci: use docker build in actions
    
    * ci: fix dependency graph
    
    * test: fix makefile and restrict support to 3.9-
    
    * test: remove 3.5/6 builds and add 3.9 wheel

--- setup.py
+++ setup.py
@@ -15,9 +15,8 @@ setup(
     classifiers=[
         "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
