eb44568ca2093f052d42a3eb3d829bcdcb8bd1cd
Jeremiah Matthey <jeremiah.matthey@spire.com>
2023-05-12 17:38:04 +0000

    Bump version to 1.7.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="pypredict",
-    version="1.7.0",
+    version="1.7.1",
     author="Jesse Trutna",
     author_email="jesse@spire.com",
     maintainer="Spire Global Inc",
